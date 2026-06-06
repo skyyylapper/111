1b8387c3de3c9c1ec7e382c7d0d300d9a817cba9
skyyylapper <izzqwar@mail.ru>
2026-06-06 23:14:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
 YOO_MONEY_WALLET = os.getenv("YOO_MONEY_WALLET")
-YOO_MONEY_CARDS = os.getenv("YOO_MONEY_CARDS", "")  # "Тинькофф: 1234, Сбер: 5678"
+YOO_MONEY_CARDS = os.getenv("YOO_MONEY_CARDS", "")
 URALSIB_CARD = os.getenv("URALSIB_CARD")
 
-YOO_MONEY_TOKEN = os.getenv("YOO_MONEY_TOKEN")      # OAuth-токен с правами payment-p2p, operation-history
+YOO_MONEY_TOKEN = os.getenv("YOO_MONEY_TOKEN")
+
+PROXY_URL = os.getenv("PROXY_URL", "")   # если не задано, будет пустая строка
