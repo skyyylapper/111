efd5b6e432444ac207dd4b9d597419594fc39eb6
skyyylapper <izzqwar@mail.ru>
2026-06-07 23:05:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,9 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
-YOO_MONEY_WALLET = os.getenv("YOO_MONEY_WALLET")
-YOO_MONEY_CARDS = os.getenv("YOO_MONEY_CARDS", "")
-URALSIB_CARD = os.getenv("URALSIB_CARD")
+# Списки реквизитов (через запятую)
+YOO_MONEY_WALLETS = os.getenv("YOO_MONEY_WALLETS", "")   # "41001..., 41002..."
+YOO_MONEY_CARDS = os.getenv("YOO_MONEY_CARDS", "")        # "Тинькофф: 1234, Сбер: 5678"
+URALSIB_CARDS = os.getenv("URALSIB_CARDS", "")            # "5559..., 4444..."
 
-YOO_MONEY_TOKEN = os.getenv("YOO_MONEY_TOKEN")
-
-PROXY_URL = os.getenv("PROXY_URL", "")   # если не задано, будет пустая строка
+PROXY_URL = os.getenv("PROXY_URL", "")
